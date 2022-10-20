de8fdfe859f81d67f0c1df11046ed4ed56557b66
VMH-IT <vmh.it170701@gmail.com>
2022-10-20 22:30:09 +0700

    fix route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -7,14 +7,16 @@ Rails.application.routes.draw do
         resources :sessions, only: [:create]
         resources :exercises
         resources :admins
-        resources :categories
-        get '/search_gym', to: 'categories#search_gym'
-        get '/search_cadio', to: 'categories#search_cadio'
+        resources :categories do 
+          collection do
+            get :search_gym
+            get :search_cadio
+          end
+        end
       end
       namespace :users do
         resources :users
         resources :sessions , only: [:create]
-        # resources :account_activations, only: [:edit]
         resources :exercises
 
       end
